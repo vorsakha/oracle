69602a4b5701ed0eea7c3992aaf53fda4dd400f7
vorsakha <andreitf.dev@gmail.com>
2022-09-03 18:14:39 -0300

    refactor: change the way the loading activity is shown

--- src/components/common/Loading/Loading.tsx
+++ src/components/common/Loading/Loading.tsx
@@ -1,23 +1,17 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { ActivityIndicator } from 'react-native';
 import { useTheme } from 'styled-components';
-import { MainSignalContext } from '../../../context/mainSignal';
-import { PairsContext } from '../../../context/pairs';
 import { LoadingWrapper } from './styles';
 
 function Loading() {
-  const { mainSignalLoading } = useContext(MainSignalContext);
-  const { pairsLoading } = useContext(PairsContext);
   const theme = useTheme();
 
   return (
     <LoadingWrapper>
-      {(mainSignalLoading || pairsLoading) && (
-        <ActivityIndicator
-          size="large"
-          color={theme.colors.backgroundSecondary}
-        />
-      )}
+      <ActivityIndicator
+        size="large"
+        color={theme.colors.backgroundSecondary}
+      />
     </LoadingWrapper>
   );
 }
