0be915a480d8663403baf998cefaeb026a969952
vorsakha <andreitf.dev@gmail.com>
2022-09-05 21:46:01 -0300

    style: fix navigation spacing

--- src/components/Navigation/styles.ts
+++ src/components/Navigation/styles.ts
@@ -1,11 +1,10 @@
 import styled from 'styled-components/native';
 
 export const NavigationWrapper = styled.View`
-  /* flex: 2; */
   height: 100px;
   background-color: ${({ theme }) => theme.colors.backgroundSecondary};
   border-radius: ${({ theme }) => theme.sizes.lg};
-  margin: ${({ theme }) => theme.sizes.sm};
+  margin: ${({ theme }) => theme.sizes.md};
 
   justify-content: center;
   align-items: center;
